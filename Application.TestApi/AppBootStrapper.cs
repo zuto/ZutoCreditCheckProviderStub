6749d1c682799556ec1185753fb840240e746aa0
ssunkari <sr.sunkari@gmail.com>
2014-10-31 20:52:27 +0000

    Added Get and Post Methods

---
+++ Application.TestApi/AppBootStrapper.cs
@@ -0,0 +1,16 @@
+﻿
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+
+namespace Application.TestApi
+{
+    public class AppBootStrapper :DefaultNancyBootstrapper
+    {
+        protected override void RequestStartup(TinyIoCContainer requestContainer, IPipelines pipelines, NancyContext context)
+        {
+            base.RequestStartup(requestContainer, pipelines, context);
+        }
+         
+    }
+}
---
+++ Application.TestApi/IndexModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+
+namespace Application.TestApi
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/GetData"] = parameters => Response.AsText(
+                "Here is Data");
+
+            Post["/PostData"] = parameters => Response.AsText(
+               "Data Received");
+        }
+
+    }
+}
