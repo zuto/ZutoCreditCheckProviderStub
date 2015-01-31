b6090659603dd7544d52d1b71a913d578eafaab6
ssunkari <sr.sunkari@gmail.com>
2015-01-31 22:40:56 +0000

    everted Previous Commit
    ods to set Timeout and set request counters"
    
    This reverts commit 1904acae2cc98e5ea9f30d9ba3887d25f8e1d07f.

--- Application.TestApi/IndexModule.cs
+++ Application.TestApi/IndexModule.cs
@@ -1,10 +1,4 @@
-﻿using System.Globalization;
-using System.Net;
-using System.Threading;
-using System.Web;
-using Nancy;
-using Nancy.Diagnostics;
-using Nancy.IO;
+﻿using Nancy;
 
 namespace Application.TestApi
 {
@@ -12,44 +6,12 @@ namespace Application.TestApi
     {
         public IndexModule()
         {
-            Get["/GetData"] = parameters => Response.AsText(Respond("Here is Data"));
+            Get["/GetData"] = parameters => Response.AsText(
+                "Here is Data");
 
-            Post["/PostData"] = parameters => Response.AsText(RespondToPost(Request.Body));
-
-            Post["/SetTimer/{timeout?0}"] = parameters => Response.AsText(SetTimeOut((int)parameters.timeout));
-
-            Get["/GetRequestCount"] = parameters => Response.AsText(Respond(HttpContext.Current.Cache.Get("RequestRecieved") == null ? null : HttpContext.Current.Cache.Get("RequestRecieved").ToString()));
+            Post["/PostData"] = parameters => Response.AsText(
+               "Data Received");
         }
 
-
-
-        private string SetTimeOut(int timeout)
-        {
-            HttpContext.Current.Cache.Remove("Timeout");
-            HttpContext.Current.Cache.Insert("Timeout", timeout);
-            return timeout.ToString(CultureInfo.InvariantCulture);
-        }
-
-        private string RespondToPost(RequestStream body)
-        {
-            var getRequestRecieved = HttpContext.Current.Cache.Get("RequestRecieved") == null ? 0 : int.Parse(HttpContext.Current.Cache.Get("RequestRecieved").ToString());
-            IncrementCount(getRequestRecieved);
-            int sleepTime = HttpContext.Current.Cache.Get("Timeout") == null ? 0 : int.Parse(HttpContext.Current.Cache.Get("Timeout").ToString());
-            Thread.Sleep(sleepTime);
-            return "Received Post";
-        }
-
-        private void IncrementCount(int getRequestRecieved)
-        {
-            HttpContext.Current.Cache.Remove("RequestRecieved");
-            getRequestRecieved++;
-            HttpContext.Current.Cache.Insert("RequestRecieved", getRequestRecieved);
-        }
-
-        private string Respond(string returnString)
-        {
-            Thread.Sleep(6000);
-            return returnString;
-        }
     }
 }
