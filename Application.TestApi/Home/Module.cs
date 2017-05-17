025438bbc9a6c0a34e2fcb81d135bc48ac325594
srinivas.sunkari <srinivas.sunkari@zuto.com>
2017-05-17 15:21:18 +0100

    Added the Default Route.

---
+++ Application.TestApi/Home/Module.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using Nancy;
+
+namespace Application.TestApi.Home
+{
+    public class Module : NancyModule
+    {
+        public Module()
+        {
+            Get["/"] = parameters => "Shabba " + Request.Headers["X-Forwarded-Proto"].FirstOrDefault();
+        }
+    }
+}
